13219c4b8754509eb21f33682d1b533073c98ac8
Yuta Funase <yfunase@gmail.com>
2023-05-07 16:58:45 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        email = request.form['email']
+        with open('emails.txt', 'a') as f:
+            f.write(email + '\n')
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
